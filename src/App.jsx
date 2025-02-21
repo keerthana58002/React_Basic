5f9e9ab6f399ea00733749438842de3b1f4ac8f4
keerthana58002 <keerthana05080@gmail.com>
2025-02-21 14:41:42 +0530

    Navbar using React Router

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import './App.css'
 import ProductCard from './card/ProductCard'
 import ReatYup from './card/Form'
 import FormikYupForm from './Form/Formik'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import About from './Components/About';
+import NotFound from './Components/NotFound';
+
 
 function App() {
   
@@ -11,7 +17,28 @@ function App() {
     <>
       {/* <ProductCard/> */}
       {/* <ReatYup/> */}
-      <FormikYupForm/>
+      {/* <FormikYupForm/> */}
+
+      <BrowserRouter>
+      <nav style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "15px", background: "grey", color: "white" }}>
+      <img 
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXEVAMVNf_q5jtB9_Xi0o6PNwAbGx-0_glkw&s"  
+        alt="Logo" 
+        style={{ height: "40px",borderRadius:"50px" }} 
+      />
+        <ul style={{ display: "flex", listStyle: "none", gap: "30px", margin: 0 }}>
+          <li><Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link></li>
+          <li><Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link></li>
+          <li><Link to="/contact" style={{ color: "white", textDecoration: "none" }}>Contact</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
       
     </>
     
---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+    <h2 style={{color:"red"}}>404-File or directory not Found.</h2>
+    <p>The resource you are looking for might have been removed,had its name changed,or is temporarily unavailable.</p>
+    </div>
+
+  )
+}
+
+
+export default NotFound;
